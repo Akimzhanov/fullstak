e68b469bd32d69a3572013e9ccc9d12019fc7de8
Akimzhanov <erasylakimjanov@gmail.com>
2022-12-01 20:38:29 +0600

    cache, log, filter

--- config/settings.py
+++ config/settings.py
@@ -73,6 +73,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
+    'middleware.CheckTime',
 ]
 
 ROOT_URLCONF = 'config.urls'
