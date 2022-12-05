432b8fbe5eec42964e0cb54ba1f7a68f684f7e07
Akimzhanov <erasylakimjanov@gmail.com>
2022-12-05 20:20:49 +0600

    fix migrate

--- config/settings.py
+++ config/settings.py
@@ -228,10 +228,10 @@ AUTH_USER_MODEL = 'account.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    # 'PAGE_SIZE':4,
-    # 'SEARCH_PARAM': 'q'
+    ),
+    #  'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    #  'PAGE_SIZE':4,
+    #  'SEARCH_PARAM': 'q'
 }
 
 SIMPLE_JWT = {
