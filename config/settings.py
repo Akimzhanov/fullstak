2feadaef54d30591bbe2c4b6607f112ae8f2be7d
dastish1411 <dastisch123@gmail.com>
2022-12-05 20:09:46 +0600

    fix

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'config/static'),
+]
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
